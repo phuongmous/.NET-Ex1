82a70afa8fc719d94531f53497d814fe234e8458
Phuong Vu <phuongmous@gmail.com>
2024-04-03 09:04:06 +0700

    Do ex2 and ex3

--- .Net Lv1/Program.cs
+++ .Net Lv1/Program.cs
@@ -1,52 +1,141 @@
 ﻿using System;
-class Person
+//Exercise 1:
+//class Person
+//{
+//    public void Greet()
+//    {
+//        Console.WriteLine("Hello!");
+//    }
+//    protected int Age { get; set; }
+//    public void SetAge(int age)
+//    {
+//        Age = age;
+//    }
+//}
+
+//class Student: Person
+//{
+//    public void Study()
+//    {
+//        Console.WriteLine("I'm studying");
+//    }
+//    public void ShowAge()
+//    {
+//        Console.WriteLine($"My age is: {Age} years old");
+//    }
+//}
+
+//class Teacher: Person
+//{
+//    public void Explain()
+//    {
+//        Console.WriteLine("I'm explaining");
+//    }
+//}
+
+//class StudentProfessorTest
+//{
+//    static void Main(string[] args)
+//    {
+//        Person person = new Person();
+//        person.Greet();
+
+//        Student student = new Student();
+//        student.SetAge(20);
+//        student.Greet();
+//        student.ShowAge();
+
+//        Teacher teacher = new Teacher();
+//        teacher.SetAge(24);
+//        teacher.Greet();
+//        teacher.Explain();
+//    }
+//}
+
+//Exercise 2:
+//abstract class Animal
+//{
+//    protected string Name { get; set; } = "";
+//    public void SetName(string name)
+//    {
+//        Name = name;
+//    }
+//    public string GetName()
+//    {
+//        return Name;
+//    }
+//    public abstract void Eat();
+//}
+
+//class Dog: Animal
+//{
+//    public override void Eat()
+//    {
+//        Console.WriteLine($"{Name} is eating");
+//    }
+//}
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.Write("Enter the name of the dog: ");
+//        string dogName = Console.ReadLine();
+//        Dog dog = new Dog();
+//        dog.SetName(dogName);
+//        Console.WriteLine($"Dog's name is: {dog.GetName()}");
+//        dog.Eat();
+//    }
+
+//}
+
+//Exercise 3:
+interface IVehicle
 {
-    public void Greet()
+    void Drive();
+    bool Refuel(int amount);
+}
+
+class Car
+{
+    private int gasolineLevel;
+    public Car(int startingGasoline)
     {
-        Console.WriteLine("Hello!");
+        gasolineLevel = startingGasoline;
     }
-    protected int Age { get; set; }
-    public void SetAge(int age)
+    public void Drive()
     {
-        Age = age;
+        if (gasolineLevel > 0)
+        {
+            Console.WriteLine("The car is driving");
+        }
+        else
+        {
+            Console.WriteLine("Cannot drive. Gasoline level is 0");
+        }
+    }
+    public bool Refuel(int amount)
+    {
+        if (amount <= 0)
+        {
+            Console.WriteLine("Invalid amount of gasoline to refuel!");
+            return false;
+        }
+        gasolineLevel += amount;
+        Console.WriteLine($"Refuel {amount} units of gasoline. The current gasoline level is {gasolineLevel}");
+        return true;
     }
 }
 
-class Student: Person
-{
-    public void Study()
-    {
-        Console.WriteLine("I'm studying");
-    }
-    public void ShowAge()
-    {
-        Console.WriteLine($"My age is: {Age} years old");
-    }
-}
-
-class Teacher: Person
-{
-    public void Explain()
-    {
-        Console.WriteLine("I'm explaining");
-    }
-}
-
-class StudentProfessorTest
+class Program1
 {
     static void Main(string[] args)
     {
-        Person person = new Person();
-        person.Greet();
-
-        Student student = new Student();
-        student.SetAge(20);
-        student.Greet();
-        student.ShowAge();
-
-        Teacher teacher = new Teacher();
-        teacher.SetAge(24);
-        teacher.Greet();
-        teacher.Explain();
+        Car car = new Car(0);
+        Console.Write("Enter an amount of gasoline to refuel: ");
+        int amountToRefuel = Convert.ToInt32(Console.ReadLine());
+        car.Refuel(amountToRefuel);
+        car.Drive();
     }
+
 }
